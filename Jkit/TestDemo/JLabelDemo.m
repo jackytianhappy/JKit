d32d804d99679de443d493499ce3a28134e6fe5d
jackytianhappy <1142899950@qq.com>
2016-10-10 14:24:35 +0800

    添加信息展示的例子

--- Jkit/TestDemo/JLabelDemo.m
+++ Jkit/TestDemo/JLabelDemo.m
@@ -15,11 +15,24 @@
     if (self = [super initWithFrame:frame]) {
         JLabel *lbl = [[JLabel alloc]initWithFrame:CGRectMake(0, 100, 100, 40)];
         lbl.wrapMode = AdjustConfigLine;
-        lbl.adjustMoreLineNum = 3;
+        lbl.adjustMoreLineNum = 4;
         lbl.text = @"qrqwrqrqwrwqrwqrwrwqrqwrweqrwqerwqrwqrweqrwerwrweqrwq";
-        [lbl drawTheLabel];
-        
+        [lbl drawTheLabel];  //属性配置好了进行绘画UILable
         [self addSubview:lbl];
+        
+        JLabel *lbl1 = [[JLabel alloc]initWithFrame:CGRectMake(0, 200, 100, 40)];
+        lbl1.wrapMode = OneLine;
+        lbl1.text = @"qrqwrqrqwrwqrwqrwrwqrqwrweqrwqerwqrwqrweqrwerwrweqrwq";
+        [lbl1 drawTheLabel];
+        [self addSubview:lbl1];
+        
+        JLabel *lbl2 = [[JLabel alloc]initWithFrame:CGRectMake(0, 300, 100, 40)];
+        lbl2.wrapMode = MoreLine;
+        lbl2.text = @"qrqwrqrqwrwqrwqrwrwqrqwrweqrwqerwqrwqrweqrwerwrweqrwq";
+        [lbl2 drawTheLabel];
+        [self addSubview:lbl2];
+        
+        
     }
     return self;
 }
